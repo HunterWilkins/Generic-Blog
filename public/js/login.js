bd04c1698c77a7bd621cd3362e6e9ca4f30ac3eb
HunterWilkins <wilkins.hunter@gmail.com>
2021-05-29 11:42:45 -0500

    Added client-side signup functionality

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,7 @@
 
 const form = document.querySelector("#login");
+const toggleButton = document.querySelector("#toggle");
+let isLogin = true;
 
 function loginHandler(event) {
     event.preventDefault();
@@ -20,7 +22,46 @@ function loginHandler(event) {
             document.location.replace("/");
         }
     });
-
 }
 
-form.addEventListener("submit", loginHandler);
+function signupHandler(event) {
+    event.preventDefault();
+
+    const data = {
+        username: document.querySelector("input[name=username]").value,
+        email: document.querySelector("input[name=email]").value,
+        password: document.querySelector("input[type=password]").value
+    }
+
+    fetch("/api/users/signup", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(data)
+    }).then(response => {
+        if (response.ok) {
+            document.location.replace("/");
+        }
+    });
+}
+
+form.addEventListener("submit", function(event) {
+    if (isLogin) {
+        loginHandler(event);
+    }
+    else {
+        signupHandler(event);
+    }
+});
+
+toggleButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    isLogin = !isLogin;
+    if (!isLogin) {
+        document.querySelector("input[name=email]").setAttribute("style", "display:block");
+    }
+    else {
+        document.querySelector("input[name=email]").setAttribute("style", "display:none");
+    }
+})
